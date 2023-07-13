0635cfba6b4c7d53b59acded8874f6dd4d863215
Luis Eduardo <luis_cg.github@hotmail.com>
2023-07-13 20:06:46 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department d_test = new Department(1, "Books");
 		
 		Seller seller = new Seller(1, "Luis", "luis@gmail.com", LocalDate.now(), 3000.00, d_test);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 
 	}
