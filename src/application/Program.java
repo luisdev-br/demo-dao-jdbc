a1c0e859ab087c5cdacd67253f24983f163f2b7e
Luis Eduardo <luis_cg.github@hotmail.com>
2023-07-13 19:49:15 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department d_test = new Department(1, "Books");
-		System.out.println(d_test);
+		
+		Seller seller = new Seller(1, "Luis", "luis@gmail.com", LocalDate.now(), 3000.00, d_test);
+		System.out.println(seller);
 
 	}
 
