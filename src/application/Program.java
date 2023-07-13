a2a34bc906fcca58a2dd6663b321367d033def18
Luis Eduardo <luis_cg.github@hotmail.com>
2023-07-13 20:32:05 -0300

    findById implement

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,18 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department d_test = new Department(1, "Books");
-		
-		Seller seller = new Seller(1, "Luis", "luis@gmail.com", LocalDate.now(), 3000.00, d_test);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		System.out.println(seller);
+		
+		Seller s1 = sellerDao.findById(3);
+		System.out.println(s1);
 
 	}
 
